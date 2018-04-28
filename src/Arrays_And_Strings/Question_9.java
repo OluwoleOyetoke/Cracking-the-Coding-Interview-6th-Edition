1cdf299778a4a43c00a2f850284b01e7330f39e9
OluwoleOyetoke <oluwoleoyetoke@gmail.com>
2018-04-28 19:27:03 +0200

    Add answer to arrays and string question 9

---
+++ src/Arrays_And_Strings/Question_9.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2018 Oluwole Oyetoke <oluwoleoyetoke@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package Arrays_And_Strings;
+
+/**
+ * <b>String Rotation:</b> Assume you have a method isSubstringwhich checks if
+ * oneword is a substring of another. Given two strings, sl and s2, write code
+ * to check if s2 is a rotation of sl using only one call to isSubstring (e.g.,
+ * "waterbottle" is a rotation of"erbottlewat").
+ *
+ * @author Oluwole Oyetoke {@literal <}oluwoleoyetoke {@literal @}
+ */
+public class Question_9 {
+
+    public static String solve(String s1, String s2) {
+       
+        //Question not clear
+        
+        return "";
+    }
+
+    /**
+     * Question_9 main.....
+     *
+     * @param args command line arguments
+     *//*
+    public static void main(String[] args) {
+       
+    }*/
+
+}
