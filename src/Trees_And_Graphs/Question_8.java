4e112d7be18cb260d1247257a88bd9114f72c156
Oluwole Oyetoke <oluwoleoyetoke@gmail.com>
2018-06-15 12:54:37 +0200

    Update Question_8

--- src/Trees_And_Graphs/Question_8.java
+++ src/Trees_And_Graphs/Question_8.java
@@ -16,6 +16,7 @@
  */
 package Trees_And_Graphs;
 
+
 /**
  * <b>First Common Ancestor:</b> Design an algorithm and write code to find the
  * first common ancestor of two nodes in a binary tree. Avoid storing additional
@@ -26,45 +27,63 @@ package Trees_And_Graphs;
  * gmail.com{@literal >}
  */
 public class Question_8 {
+    
+    /**
+     * First check if the two values whose ancestors are to be found are present in the tree
+     * @param tree tree
+     * @param a node to find its common ancestor with b
+     * @param b node to find its common ancestor with a
+     * @throws TreeException  tree exception
+     */
+    public void solve(Bst tree, Bst.Node a, Bst.Node b) throws TreeException {
+        //check if tree contains a and b
+        boolean check1 = tree.search(a.value);
+        boolean check2 = tree.search(b.value);
+        if (check1 == false || check2 == false) {
+            System.out.println("Common ancestor not calculated. One of the numbers not in tree");
+            return;
+        }
+        findCommonAncestor(tree.root, a, b);
+    }
 
-    
-    
-    
-    
-      /**
+    /**
      * In order traversal of BSt to find common ancestor
+     *
      * @param root root to start traversal from
      * @param a node to find its common ancestor with b
      * @param b node to find its common ancestor with a
      */
     public void findCommonAncestor(Bst.Node root, Bst.Node a, Bst.Node b) {
-        if (root == null || a==null || b==null ) {
+        if (root == null || a == null || b == null) {
             return;
         }
-        if(a==b){
-             System.out.println("Common ancestot for :"+a.value+" and "+b.value+" = "+root.value);
-             return;
+        if (a.value == b.value) {
+            if(root.value == a.value){
+                System.out.println("Same: Common ancestor for :" + a.value + " and " + b.value + " = " + root.ancestor.value);
+               return;
+            } 
         }
-        int bigger = (a.value>b.value)?a.value:b.value;
-         int smaller = (a.value<=b.value)?a.value:b.value;
-        
-        findCommonAncestor(root.left,a,b);
-        //assuming no duplicate values
-        if(root.left!=null && smaller<=root.left.value && root.right!=null && bigger>root.right.value){
-         System.out.println("Common ancestot for :"+a.value+" and "+b.value+" = "+root.value);   
-        }
-        
-        findCommonAncestor(root.right,a,b);
 
+        //assuming no duplicate values
+        if (a.value < root.value && b.value > root.value) {
+            System.out.println("Common ancestot for :" + a.value + " and " + b.value + " = " + root.value);
+            return;
+        } else if (b.value < root.value && a.value > root.value) {
+            System.out.println("Common ancestot for :" + a.value + " and " + b.value + " = " + root.value);
+            return;
+        }
+        findCommonAncestor(root.left, a, b);
+        findCommonAncestor(root.right, a, b);
     }
-        
+
     /**
-     * Bst main/test method....uncomment to run
+     * Trees and graphs question_8 main/test method....uncomment to run
+     *
      * @param args command line arguments
      * @throws TreeException Tree Exception
-     */
+     *//*
     public static void main(String[] args) throws TreeException {
-        //LOAD AND LOAD BST
+        //LOAD BST
         Bst bst = new Bst(7);
         for (int i = 0; i < 8; i++) {
             Integer toAdd = i * 2;
@@ -73,9 +92,10 @@ public class Question_8 {
         bst.add(9);
         bst.add(11);
         bst.add(1);
-        Bst.Node a = new Bst.Node(11);
-        Bst.Node b = new Bst.Node(1);
+        Bst.Node a = new Bst.Node(1);
+        Bst.Node b = new Bst.Node(14);
         Question_8 q8 = new Question_8();
-        q8.findCommonAncestor(bst.root, a, b);
+        q8.solve(bst, a, b);
     }
+*/
 }
