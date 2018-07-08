bffa49b5fd37818500217f6119640023a6a8094d
OluwoleOyetoke <oluwoleoyetoke@gmail.com>
2018-07-08 23:09:04 +0200

    Add OOP Parking Lot Solution

---
+++ src/Object_Oriented_Design/ParkingLot/Driver.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2018 Oluwole Oyetoke <oluwoleoyetoke@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package Object_Oriented_Design.ParkingLot;
+
+/**
+ * Driver Class
+ * @author Oluwole Oyetoke {@literal <}oluwoleoyetoke {@literal @}
+ * gmail.com{@literal >}
+ */
+public class Driver {
+
+    String name;
+    Vehicle car;
+    
+    Driver(String name) {
+        this.name = name;
+    }
+
+    Driver(String name, Vehicle car) {
+        this.name = name;
+        this.car = car;
+        car.setDriver(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vehicle getCar() {
+        return car;
+    }
+
+    public void setCar(Vehicle car) {
+        this.car = car;
+    }
+
+}
---
+++ src/Object_Oriented_Design/ParkingLot/Test.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2018 Oluwole Oyetoke <oluwoleoyetoke@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package Object_Oriented_Design.ParkingLot;
+
+/**
+ * <b>Parking Lot: </b>Design a parking lot using object-oriented principles
+ * Assumptions:
+ * <ol>
+ * 
+ * <li>Vehicle = Car, Moto Cycle,Busses and Trailers</li>
+ * <li>All vehicles can park anywhere that is available<li>
+ * <li>The car park has only one level</li>
+ * <li>The car park space is generic. No special preferences for disabled parking</li>
+ * <li>All vehicles need equal parking space i.e 1 unit</li>
+ * </ol>
+ *
+ * @author Oluwole Oyetoke {@literal <}oluwoleoyetoke {@literal @}
+ * gmail.com{@literal >}
+ */
+public class Test {
+
+    /**
+     * Parking Lot OOP main method.....Uncomment to run
+     *
+     * @param args command line arguments
+     *//*
+    public static void main(String[] args){
+        Driver driver = new Driver("Jason Statham");
+        Vehicle car = new Vehicle("JBenz", driver);
+        Vehicle car2 = new Vehicle("NToyota", driver);
+        Lot parkingLot = new Lot("Central London", 5);
+        
+        parkingLot.parkIn(car);
+        parkingLot.parkIn(car2);
+        parkingLot.driveOut(car);
+        parkingLot.parkIn(car2);
+        parkingLot.parkIn(car);  
+    }*/ 
+}
