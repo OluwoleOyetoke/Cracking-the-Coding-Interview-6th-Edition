1518f4bdd0a3c9143a57ac5d418aa63d4fde0013
OluwoleOyetoke <oluwoleoyetoke@gmail.com>
2018-08-07 02:22:28 +0200

    Add JUnit Test Sample

---
+++ test/Others/PrimesTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2018 Oluwole Oyetoke <oluwoleoyetoke@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package Others;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Oluwole Oyetoke - oluwoleoyetoke at gmail.com
+ */
+public class PrimesTest {
+    
+    public PrimesTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of findPrimes method, of class Primes.
+     */
+    @Test
+    public void testFindPrimes() {
+        System.out.println("Testing findPrimes(int m)\n");
+        int m = 0;
+        Primes instance = new Primes();
+        instance.findPrimes(m);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
