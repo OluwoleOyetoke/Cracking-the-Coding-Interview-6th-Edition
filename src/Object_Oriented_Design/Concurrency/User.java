8997d576ccec6d385e28b2462f902e4d22a17471
OluwoleOyetoke <oluwoleoyetoke@gmail.com>
2018-08-15 17:30:21 +0200

    Add Concurrency Sample with - [Thread Pool Executor, Semaphore, Locks, Executor Service etc]

---
+++ src/Object_Oriented_Design/Concurrency/User.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2018 Oluwole Oyetoke <oluwoleoyetoke@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package Object_Oriented_Design.Concurrency;
+
+/**
+ * Test class
+ * @author Oluwole Oyetoke - oluwoleoyetoke at gmail.com            
+ */
+public class User {
+    
+    /*
+    public static void main(String[] args){
+        Scheduler scheduler= Scheduler.getInstance();
+        
+        for(int i=0; i<20; i++){
+           scheduler.execute(new Task(i+"")); 
+        } 
+    }
+*/
+}
